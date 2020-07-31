3e43d088f53ec99640069e0549cf89ed1343864c
Damjan011 <damjanpiperski@gmail.com>
2020-07-31 22:50:58 +0200

    Accounts page route and container with 4 components. Components are pretty big so it is not perfect but i wasn't sure how to organize it in a different way. Some CSS is overlapping, we need to talk about that

--- src/App.js
+++ src/App.js
@@ -2,46 +2,52 @@ import React from 'react';
 import Home from './containers/home';
 import Subscribers from './containers/subscribers';
 import Networks from './containers/networks';
+import Accounts from './containers/accounts';
 import {
-    BrowserRouter as Router,
-    Switch,
-    Route,
-    Link
-  } from "react-router-dom";
-  
-  export default function App() {
-    return (
-      <Router>
-        <div>
-          <nav>
-            <ul>
-              <li>
-                <Link to="/">Home</Link>
-              </li>
-              <li>
-                <Link to="/subscribers">Subscribers</Link>
-              </li>
-              <li>
-                <Link to="/networks">Networks</Link>
-              </li>
-            </ul>
-          </nav>
-  
-          {/* A <Switch> looks through its children <Route>s and
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+
+export default function App() {
+  return (
+    <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/subscribers">Subscribers</Link>
+            </li>
+            <li>
+              <Link to="/networks">Networks</Link>
+            </li>
+            <li>
+              <Link to="/accounts">Accounts</Link>
+            </li>
+          </ul>
+        </nav>
+
+        {/* A <Switch> looks through its children <Route>s and
               renders the first one that matches the current URL. */}
-          <Switch>
-            <Route path="/subscribers">
-              <Subscribers />
-            </Route>
-            <Route path="/networks">
-              <Networks />
-            </Route>
-            <Route path="/">
-              <Home />
-            </Route>
-          </Switch>
-        </div>
-      </Router>
-    );
-  }
-  
+        <Switch>
+          <Route path="/accounts">
+            <Accounts />
+          </Route>
+          <Route path="/subscribers">
+            <Subscribers />
+          </Route>
+          <Route path="/networks">
+            <Networks />
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
+  );
+}
---
+++ src/containers/accounts/index.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import './accounts.css';
+import AccountsHeader from '../../components/accountsHeader';
+import ActiveAccountsList from '../../components/activeAccountsList';
+import AccountOrganizationsList from '../../components/accountOrganizationsList';
+import SwitchAccountsList from '../../components/switchAccountsList';
+
+function Accounts() {
+  return (
+    <div className="ui-layout">
+      <div className="ui-top-content">
+        <AccountsHeader />
+        <div className="ui-account-wrapper">
+          <ActiveAccountsList />
+          <AccountOrganizationsList />
+          <SwitchAccountsList />  
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Accounts;
