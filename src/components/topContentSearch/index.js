c9cd8865b136fe605414a8dc8c948ab09b73a7bf
Damjan011 <damjanpiperski@gmail.com>
2020-08-05 14:07:54 +0200

    Subscribers page, top icon boxes made as components

--- src/components/topContentSearch/index.js
+++ src/components/topContentSearch/index.js
@@ -1,14 +1,10 @@
 import './style.css';
 import React from 'react';
-import HeaderIcons from '../../components/headerIcons';
 import SearchBar from '../../components/searchBar';
-import HeaderLabels from '../headerLabels';
 
 const TopContentSearch = ({ }) => (
   <div className="ui-top-content-search">
     <div className="ui-top-header-search">
-      <HeaderLabels />
-      <HeaderIcons />
     </div>
     <SearchBar />
   </div>
