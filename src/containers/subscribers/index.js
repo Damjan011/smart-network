437483d049ede5803116769df6665238551ada45
Damjan011 <damjanpiperski@gmail.com>
2020-08-05 20:54:23 +0200

    Subscribers page, support info box made as a component

--- src/containers/subscribers/index.js
+++ src/containers/subscribers/index.js
@@ -4,6 +4,7 @@ import SubscribersMap from '../../components/subscribersMap';
 import SubscribersUtilization from '../../components/subscribersUtilization';
 import SubscribersHealthBars from '../../components/subscribersHealthBars';
 import SubscribersMenu from '../../components/subscribersMenu';
+import SubscribersSupportInfo from '../../components/subscribersSupportInfo';
 
 function Subscribers() {
   return (
@@ -25,29 +26,20 @@ function Subscribers() {
       <div className="ui-center-content">
         <SubscribersUtilization />
         <SubscribersHealthBars />
-        <div id="bandwidth-usage " className="ui-box ">
+        <div id="bandwidth-usage" className="ui-box ">
           <h1>Bandwidth usage<br /> Graph</h1>
         </div>
-        <div id="data-transfer " className="ui-box ">
+        <div id="data-transfer" className="ui-box ">
           <h1>Data transfer<br /> Graph</h1>
         </div>
-        <div id="health " className="ui-box ">
+        <div id="health" className="ui-box ">
           <h1>Health<br /> Graph</h1>
         </div>
         <div className="ui-column-break ">
         </div>
         <SubscribersMap />
         <SubscribersMenu />
-        <div id="support-info " className="vertical-menu-right ui-box ">
-          <a className="ui-blue-item " href="# ">
-            <p>Support Info</p>
-            <p>Hide</p>
-          </a>
-          <p className="ui-support-info-text ">Lorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsum</p>
-          <a href="# ">
-            <p className="ui-blue-text ">Share...</p>
-          </a>
-        </div>
+        <SubscribersSupportInfo/>
       </div>
     </div>);
 }
