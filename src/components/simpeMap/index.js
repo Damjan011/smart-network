658836baf34b3776508ac7ed3d9cb2f11099bc51
Damjan011 <damjanpiperski@gmail.com>
2020-09-09 19:29:40 +0200

    Switched to new map library, new logic for markers and Info Window. Created new Info Window as a custom component, UI is included from infoWindowContent folder. Copy both CSS files

--- src/components/simpeMap/index.js
+++ src/components/simpeMap/index.js
@@ -1,82 +1,39 @@
-import { Map, Marker, GoogleApiWrapper } from 'google-maps-react';
 import React from 'react';
+import GoogleMapReact from 'google-map-react';
 import './style.css';
 import InfoWindowContent from './infoWindowContent';
-import InfoWindowEx from './infoWindowEx';
+
+const InfoWindow = props => (
+  <InfoWindowContent />
+);
+
+const AnyReactComponent = ({ text }) => (
+  <div className="info-window-container">
+    <img src={require('../../assets/images/bridge-green-marker.png')} style={{ width: '30px', height: '34px' }} alt="Map marker" />
+    <InfoWindow />
+  </div>
+);
 
 export class MapContainer extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      acknowledge: false
-    }
-  };
-
-  onWindowChange(acknowledge) {
-    this.setState(acknowledge);
-  };
-
-  state = {
-    showingInfoWindow: false,
-    activeMarker: {},
-    selectedPlace: {},
-  };
-
-  onMarkerClick = (props, marker, e) =>
-    this.setState({
-      selectedPlace: props,
-      activeMarker: marker,
-      showingInfoWindow: true
-    });
-
-  onMapClicked = (props) => {
-    if (this.state.showingInfoWindow) {
-      this.setState({
-        showingInfoWindow: false,
-        activeMarker: null
-      })
-    }
-  };
-
   render() {
     return (
-      <Map
-        google={this.props.google}
-        onClick={this.onMapClicked}
-        zoom={14}
-        initialCenter={{ lat: 44.776930, lng: 20.476100 }}>
-
-        <Marker onClick={this.onMarkerClick}
-          title={'Cafe Liberte'}
-          name={'Liberte'}
-          position={{ lat: 44.776319, lng: 20.475316 }} />
-
-        <Marker onClick={this.onMarkerClick}
-          title={'Cafe Escalera'}
-          name={'Escalera'}
-          position={{ lat: 44.784198, lng: 20.475534 }} />
-
-        <Marker onClick={this.onMarkerClick}
-          title={'Cafe Kangaroo'}
-          name={'Kangaroo'}
-          position={{ lat: 44.785436, lng: 20.468537 }} />
-
-        <Marker onClick={this.onMarkerClick}
-          title={'Auto parts store'}
-          name={'Euro Auto'}
-          position={{ lat: 44.785710, lng: 20.467780 }} />
-
-        <InfoWindowEx className='ui-info-window'
-          marker={this.state.activeMarker}
-          visible={this.state.showingInfoWindow}>
-          <InfoWindowContent />
-        </InfoWindowEx>
-
-      </Map>
+      <div style={{ height: '100vh', width: '100%' }}>
+        <GoogleMapReact
+          bootstrapURLKeys={{ key: "AIzaSyClhpytnSsGpLP5r6MFq05qM3iHHkuSSCI" }}
+          defaultCenter={{
+            lat: 38.1715307692773,
+            lng: -121.93831743140169
+          }}
+          zoom={9}
+          yesIWantToUseGoogleMapApiInternals>
+            <AnyReactComponent
+              lat="38.1715307692773"
+              lng="-121.93831743140169"
+              text="JOOOJ DEBILU"/>
+        </GoogleMapReact>
+      </div>
     );
   }
 }
 
-export default GoogleApiWrapper({
-  apiKey: "AIzaSyClhpytnSsGpLP5r6MFq05qM3iHHkuSSCI"
-})(MapContainer)
+export default MapContainer;
